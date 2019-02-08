45d0cc5c51b172cfba4ed1fc43658d1710e8399a
Jimmy Arroyo <jamesarroyo5@gmail.com>
2019-02-08 11:12:58 -0700

    Implemented the representative list component in the app component and styled it to take up half the width.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import './App.scss';
 import HeaderBar from './components/header-bar/header-bar';
+import RepresentativesList from './representatives/representatives-list/representatives-list';
 
 class App extends Component {
   render() {
     return (
       <div className="App-container">
         <div className="App-main">
-          <HeaderBar title="Who's My Representative"></HeaderBar>
-
+          <div className="header">
+            <HeaderBar title="Who's My Representative"></HeaderBar>
+          </div>
+          <div className="rep-list">
+            <RepresentativesList></RepresentativesList>
+          </div>
         </div>
       </div>
     );
