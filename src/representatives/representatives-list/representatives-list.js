0b79325aa972d5be2550e7973a23e83271a51789
Jimmy Arroyo <jamesarroyo5@gmail.com>
2019-02-08 11:11:18 -0700

    Implemented the rep table component in the rep list component using fake data.

--- src/representatives/representatives-list/representatives-list.js
+++ src/representatives/representatives-list/representatives-list.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './representatives-list.scss';
+import Table from '../representatives-table/representatives-table';
+
+const reps = [
+  { name: 'John Appleseed', party: 'R'}, 
+  { name: 'John Cena', party: 'D'}, 
+  { name: 'John Doe', party: 'R'}, 
+  { name: 'John Snow', party: 'I'}, 
+  ];
 
 class RepresentativesList extends React.Component {
 
   render() {
-    return (<div></div>)
+    return (
+      <div className="container">
+        <h3>List / <text className="primary">Representatives</text></h3>
+        <Table data={ reps } />
+      </div>
+    )
   }
 }
 
